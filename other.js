c024a15b599d4a9893c7946d5ed0bb552485a341
张斯磊 <1055191226@qq.com>
2016-06-12 18:18:21 +0800

    添加：阻止事件冒泡方法

--- other.js
+++ other.js
@@ -63,6 +63,17 @@ function clickEvent() {
 	return ('ontouchend' in document.documentElement) ? 'touchend' : 'click';
 }
 
+/**
+ * 阻止事件冒泡
+ */
+function stopPropagation(e) {
+  e = e || window.event;
+  if(e.stopPropagation) {    //W3C阻止冒泡方法
+    e.stopPropagation();
+  } else {
+    e.cancelBubble = true;    //IE阻止冒泡方法
+  }
+}
 
 ///////////////////////////   其他   ///////////////////////////
 
